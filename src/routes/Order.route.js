0b31fdd935c616784ae4719ce6c35959b7d120c8
vrajpatel0007 <vraj40983@gmail.com>
2024-03-04 12:45:50 +0530

    auth

--- src/routes/Order.route.js
+++ src/routes/Order.route.js
@@ -1,19 +1,26 @@
 const express = require("express");
-const Order_controller = require("../controllers/Order.controller")
+const Order_controller = require("../controllers/Order.controller");
 const { autheticate, restrict } = require("../middleware/auth");
 const routes = express.Router();
 
 // create-Order
-routes.post("/create-Order",Order_controller.createOrder)
+routes.post("/create-Order", autheticate, Order_controller.createOrder);
 
 /** Get Order list */
-routes.get("/list",Order_controller.getOrderList);
+routes.get("/list", autheticate, Order_controller.getOrderList);
 
 /** Delete Order */
-routes.delete("/delete-Order/:OrderId",Order_controller.deleteOrder);
+routes.delete(
+  "/delete-Order/:OrderId",
+  autheticate,
+  Order_controller.deleteOrder
+);
 
 // Update Order
-routes.put("/update-details/:OrderId",Order_controller.updateDetails);
-
+routes.put(
+  "/update-details/:OrderId",
+  autheticate,
+  Order_controller.updateDetails
+);
 
 module.exports = routes;
